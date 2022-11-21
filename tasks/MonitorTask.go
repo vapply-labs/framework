a985f9c27c7f46a12f089200f450e0c47ac158ff
Joseph Chen <jchen42703@gmail.com>
2022-11-21 02:05:37 -0500

    Basic JobMonitorTask interface

--- tasks/MonitorTask.go
+++ tasks/MonitorTask.go
@@ -1,1 +1,10 @@
 package tasks
+
+// Monitors a careers website, and when it finds a new job, call "OnNewJob"
+type JobMonitorTask interface {
+	// Scrapes the website content to parse new jobs.
+	Scrape() []*Job
+
+	// Call this callback for each new job found.
+	OnNewJob(job *Job) error
+}
