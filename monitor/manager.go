b2621fbce2671e266c6ec3b04d21209418c2f052
Joseph Chen <jchen42703@gmail.com>
2022-11-21 02:16:21 -0500

    Added CreateMonitorTasks to MonitorsManager

--- monitor/manager.go
+++ monitor/manager.go
@@ -1,8 +1,14 @@
 package monitor
 
-import "github.com/vapply-labs/framework/jobs"
+import (
+	"github.com/vapply-labs/framework/jobs"
+	"github.com/vapply-labs/framework/tasks"
+)
 
 type MonitorsManager interface {
+	// Creates a monitor task for each given company.
+	CreateMonitorTasks(companies []*jobs.Job) []*tasks.JobMonitorTask
+
 	// Start a monitor task for each given company.
 	StartMonitorTasks(companies []*jobs.Job) error
 
